2816231ed550cec1252f0bbe43f1fabc9e0b1297
kskwisz28 <kskwisz2853@gmail.com>
2022-09-07 23:37:10 +0200

    working on about us page

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -1,3 +1,51 @@
 <x-main-layout>
 
+    <div class="mt-14">
+        <div class="mx-auto text-center text-5xl tracking-wider text-zinc-900 font-normal flex justify-center">
+            <h1 class="text-5xl font-bold text-zinc-900 mb-12">
+                About <span class="text-primary">us</span>
+            </h1>
+        </div>
+    </div>
+
+    <section>
+        <div class="py-10 mx-auto">
+            <div class="grid grid-cols-1 lg:grid-cols-2">
+                <div class="relative z-10 bg-primary">
+                    <div class="relative lg:h-full">
+                        <img class="inset-0 max-h-[26em] ml-auto m-16" src="/img/team-working.svg"/>
+                    </div>
+                </div>
+
+                <div class="relative flex items-center bg-zinc-100">
+                    <div class="p-8 sm:p-16 lg:p-24 text-left">
+                        <div class="text-5xl mb-3">We want to offer a</div>
+
+                        <div class="text-zinc-900">
+                            <div class="font-bold text-6xl mb-2">
+                                <span class="text-primary">performance</span>-only
+                            </div>
+
+                            <div class="font-bold text-7xl mb-5">
+                                <span class="text-primary">SEO</span> service
+                            </div>
+                        </div>
+
+                        <div class="text-xl tracking-wider max-w-lg">
+                            especially catering to smaller businesses who compete against much larger firms
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <x-container class="font-normal text-base leading-[1.7rem]">
+        <h2 class="mb-4 text-4xl font-medium">Company</h2>
+
+        <p class="mb-5">AutoRanker was founded in 2019 and serves a bit over 200 mostly small and mid-sized businesses. Prior to AutoRanker, a member of our team spent six years implementing autocomplete-optimization projects for a small client base and this way, gained the necessary experience for AutoRanker.</p>
+        <p class="mb-5">As our client, you benefit from almost 8 years of experience and hundreds of previously completed projects. We are your specialist when it comes to positioning your brand in the autocomplete function of search engines and we would love to work with you.</p>
+        <p class="mb-5">We always conduct our work in a transparent and organic manner. We think long-term, plan painstakingly and support our clients in the creation, controlling and adjustments of their campaigns. Our own keyword database, a reporting portal and e-mail reports compliment our offerings.</p>
+        <p class="mb-5">When implementing a campaign we hold ourselves to high standards - both the commercial success of our clients and the value-add to end users who interact with the suggests we manage. We only accept projects we personally believe in and which offer a sustained positive experience to end users.</p>
+    </x-container>
 </x-main-layout>
--- resources/views/success_stories.blade.php
+++ resources/views/success_stories.blade.php
@@ -1,5 +1,11 @@
 <x-main-layout>
-
+    <div class="my-14">
+        <div class="mx-auto text-center text-5xl tracking-wider text-zinc-900 font-normal flex justify-center">
+            <h1 class="text-5xl font-bold text-zinc-900 mb-12">
+                Success <span class="text-primary">stories</span>
+            </h1>
+        </div>
+    </div>
 
     <div>
         <x-search-keyword-or-domain></x-search-keyword-or-domain>
