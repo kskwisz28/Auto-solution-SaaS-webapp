c9c497e929c65587d7ce0eaebd2f73a744049249
kskwisz28 <kskwisz2853@gmail.com>
2022-09-15 12:36:16 +0200

    work on checkout page

--- resources/views/components/title.blade.php
+++
@@ -1,5 +0,0 @@
-<div {{ $attributes->merge(['class' => 'mx-auto text-center text-5xl tracking-wider text-zinc-900 font-normal flex justify-center']) }}>
-    <h1 class="text-4xl lg:text-5xl font-bold text-zinc-900 mb-4 lg:mb-8 xl:mb-12">
-        {{ $slot }}
-    </h1>
-</div>
