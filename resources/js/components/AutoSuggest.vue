bd87e26887815e5718a6a446c6932b74ac2c6032
kskwisz28 <kskwisz2853@gmail.com>
2022-09-30 18:35:39 +0200

    update on task #23

--- resources/js/components/AutoSuggest.vue
+++ resources/js/components/AutoSuggest.vue
@@ -9,6 +9,7 @@
                @keydown.down.prevent="moveSelectionDown"
                @keydown.ctrl.space="open"
                @keyup.esc="close"
+               ref="input"
                type="text"
                class="input input-lg h-[60px] w-full ring-1 ring-gray-300 px-4 md:px-6 hover:ring-2 hover:ring-primary/50 focus:ring-2 focus:ring-primary/50 focus:outline-none"/>
 
@@ -18,25 +19,15 @@
             <li v-for="(item, index) in items"
                 :key="`suggestion-${index}`"
                 @click="selectSuggestion(index)"
-                class="group flex items-center px-4 py-4 cursor-pointer"
+                class="group flex items-center px-4 py-3 cursor-pointer"
                 :class="{
-                        'hover:bg-zinc-100': index !== activeIndex,
-                        'bg-blue-50/75 text-zinc-900 hover:bg-blue-50': isActive(item, index),
-                        'bg-primary-50/20 text-primary hover:bg-primary-50/20': isSelected(item, index),
-                        'bg-blue-50/75 text-primary hover:bg-primary-50/40 hover:text-primary': isActiveAndSelected(item, index),
+                        'hover:bg-zinc-100': state(item, index).isIdle(),
+                        'bg-blue-50/75 text-zinc-900 hover:bg-blue-50': state(item, index).isActive(),
+                        'bg-primary-50/20 text-primary hover:bg-primary-50/20': state(item, index).isSelected(),
+                        'bg-blue-50/75 text-primary hover:bg-primary-50/40 hover:text-primary': state(item, index).isActiveAndSelected(),
                     }">
 
-                <svg class="w-5 h-5 mr-4"
-                     :class="{
-                            'group-hover:text-zinc-900': index !== activeIndex,
-                            'text-zinc-900': isActive(item, index),
-                            'text-primary/90 group-hover:text-primary/90': isSelected(item, index),
-                            'text-primary': isActiveAndSelected(item, index),
-                         }"
-                     viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M16.36 14c.08-.66.14-1.32.14-2c0-.68-.06-1.34-.14-2h3.38c.16.64.26 1.31.26 2s-.1 1.36-.26 2m-5.15 5.56c.6-1.11 1.06-2.31 1.38-3.56h2.95a8.03 8.03 0 0 1-4.33 3.56M14.34 14H9.66c-.1-.66-.16-1.32-.16-2c0-.68.06-1.35.16-2h4.68c.09.65.16 1.32.16 2c0 .68-.07 1.34-.16 2M12 19.96c-.83-1.2-1.5-2.53-1.91-3.96h3.82c-.41 1.43-1.08 2.76-1.91 3.96M8 8H5.08A7.923 7.923 0 0 1 9.4 4.44C8.8 5.55 8.35 6.75 8 8m-2.92 8H8c.35 1.25.8 2.45 1.4 3.56A8.008 8.008 0 0 1 5.08 16m-.82-2C4.1 13.36 4 12.69 4 12s.1-1.36.26-2h3.38c-.08.66-.14 1.32-.14 2c0 .68.06 1.34.14 2M12 4.03c.83 1.2 1.5 2.54 1.91 3.97h-3.82c.41-1.43 1.08-2.77 1.91-3.97M18.92 8h-2.95a15.65 15.65 0 0 0-1.38-3.56c1.84.63 3.37 1.9 4.33 3.56M12 2C6.47 2 2 6.5 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"/>
-                </svg>
-                {{ item.mail_domain }}
+                <slot name="item" v-bind="{item, state: state(item, index)}"></slot>
             </li>
         </ul>
     </OnClickOutside>
@@ -58,6 +49,10 @@ export default {
             required: true,
             type: String,
             default: '',
+        },
+        selectionProperty: {
+            required: true,
+            type: String,
         },
         request: {
             required: true,
@@ -91,7 +86,7 @@ export default {
             this.request()
                 .then(({data}) => {
                     this.activeIndex = -1;
-                    this.items = data.suggestions;
+                    this.items       = data.suggestions;
                     this.open();
                 })
                 .catch(e => console.error('Failed to fetch suggestions', e));
@@ -108,7 +103,7 @@ export default {
         },
 
         selectSuggestion(index) {
-            this.$emit('update:modelValue', this.items[index].mail_domain);
+            this.$emit('update:modelValue', this.items[index][this.selectionProperty]);
             this.close();
         },
 
@@ -136,18 +131,20 @@ export default {
 
         close() {
             this.show = false;
+            this.activeIndex = 0;
         },
 
-        isSelected(item, index) {
-            return index !== this.activeIndex && item.mail_domain === this.modelValue;
+        state(item, index) {
+            return {
+                isIdle: () => index !== this.activeIndex,
+                isSelected: () => index !== this.activeIndex && item[this.selectionProperty] === this.modelValue,
+                isActiveAndSelected: () => index === this.activeIndex && item[this.selectionProperty] === this.modelValue,
+                isActive: () => index === this.activeIndex && item[this.selectionProperty] !== this.modelValue,
+            };
         },
 
-        isActiveAndSelected(item, index) {
-            return index === this.activeIndex && item.mail_domain === this.modelValue;
-        },
-
-        isActive(item, index) {
-            return index === this.activeIndex && item.mail_domain !== this.modelValue;
+        focus() {
+            this.$refs.input.focus();
         },
     },
 }
