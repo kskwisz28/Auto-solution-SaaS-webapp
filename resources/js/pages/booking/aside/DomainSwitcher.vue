6dacd25638ec75784cbd3357825a1b2aaaf4ef64
kskwisz28 <kskwisz2853@gmail.com>
2022-10-01 11:40:13 +0200

    fixed domain text in domain card on booking page

--- resources/js/pages/booking/aside/DomainSwitcher.vue
+++ resources/js/pages/booking/aside/DomainSwitcher.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="text-lg inline-block">
-        Domain: <span class="font-semibold">{{ domain }}</span>
-        <label for="domain-switcher-modal" class="cursor-pointer text-sm -mt-1 ml-2 text-primary hover:underline relative bottom-0.5">(change)</label>
+    <div class="inline-block">
+        <div class="text-base mb-1">Domain:</div>
+        <div>
+            <span class="text-xl font-semibold break-all">{{ domain }}</span>
+            <label for="domain-switcher-modal" class="cursor-pointer text-sm -mt-1 ml-2 text-primary hover:underline relative bottom-0.5">(change)</label>
+        </div>
     </div>
 </template>
 
