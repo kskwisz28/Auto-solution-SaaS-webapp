b69dadd38099bf91aae210757c7482a692f203c0
lcalderes <lcalderes@gmail.com>
2022-10-26 01:04:54 +0200

    Extend staging schema by `autoranker_keywords`, `clients`, `client_accounts`, `orders`, `search_terms`, `search_term_metrics` and `search_term_connections`.

--- database/schema/schema_partial.sql
+++ database/schema/schema_partial.sql
@@ -1,3 +1,115 @@
+CREATE TABLE `autoranker_keywords` (
+   `id` int NOT NULL AUTO_INCREMENT,
+   `order_id` int NOT NULL,
+   `keyword` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+   `domain` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `path_to_ranking_subpage` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `country` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL,
+   `monthly_fee` decimal(6,2) NOT NULL DEFAULT '0',
+   `setup_fee` decimal(6,2) NOT NULL DEFAULT '0',
+   `search_volume` int unsigned DEFAULT NULL,
+   `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   `start_date` date DEFAULT NULL,
+   `termination_date` date DEFAULT NULL,
+   `termination_confirmed` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'FALSE',
+   `termination_executed` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'FALSE',
+   `termination_reason_id` int DEFAULT NULL,
+   `termination_recorded_date` date DEFAULT NULL,
+   `setup_fee_invoiced` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'FALSE',
+   `order_fulfillment_request_date` datetime DEFAULT CURRENT_TIMESTAMP,
+   `autoranker_experiment_id` int DEFAULT NULL,
+   `recurring_month_invoiced` varchar(7) COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `top_ten_notification_sent` datetime DEFAULT NULL,
+   `top_five_notification_sent` datetime DEFAULT NULL,
+   `last_rank_reported` int DEFAULT NULL,
+   `last_website_ranking` int DEFAULT NULL,
+   `last_website_ranking_update_date` datetime DEFAULT NULL,
+   `ranking_subpage_content` varchar(15000) COLLATE utf8mb4_swedish_ci DEFAULT NULL,
+   `ranking_subpage_content_last_fetched` datetime DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   KEY `fk_autoranker_keywords_order_id` (`order_id`),
+   CONSTRAINT `fk_autoranker_keywords_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `clients` (
+   `id` int NOT NULL AUTO_INCREMENT,
+   `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+   `legal_person` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'FALSE',
+   `owner` enum('hPage Ltd.','LOGOS Performance GmbH') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'hPage Ltd.',
+   `bank_details_id` int DEFAULT NULL,
+   `vatin` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `street` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `zip` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `city` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `country` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `extra_info` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `accounting_email` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL,
+   `industry` enum('Business Consultancy', 'Business Retail', 'Consumer Goods/Services', 'Education', 'Energy', 'Event Planning', 'Finance', 'Forwarding', 'Gambling', 'Health Care', 'Human Resources', 'Information Technology', 'Insurance', 'Interior', 'Legal', 'Manufacturing', 'Marketing', 'Physical Storage', 'Printing', 'Private Investigation', 'Real Estate', 'Recycling', 'Translation', 'Travel', 'Video Production', 'OTHER') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'OTHER',
+   `source` enum('Cold Call', 'Cold Mail', 'LinkedIn', 'OTHER') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'OTHER',
+   `payment_due_days` int DEFAULT '14',
+   `payment_method` enum('wire_transfer','direct_debit','credit_card') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'wire_transfer',
+   `stripe_customer_id` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `stripe_payment_method_id` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `termination_notice_period_in_days` int DEFAULT '30',
+   `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   `employee_id` int NOT NULL,
+   `deletion_date` datetime DEFAULT NULL,
+   `send_info_emails` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'TRUE',
+   `last_info_email_sent_date` datetime DEFAULT NULL,
+   `retention_email_1_sent` datetime DEFAULT NULL,
+   `retention_email_2_sent` datetime DEFAULT NULL,
+   `retention_email_3_sent` datetime DEFAULT NULL,
+   `retention_email_4_sent` datetime DEFAULT NULL,
+   `not_canceled_mrr` float DEFAULT NULL,
+   `autoranker_bonus_email_1_sent` datetime DEFAULT NULL,
+   `accountmanager_introduction_sent` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'FALSE',
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `uidx_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `client_accounts` (
+   `id` int NOT NULL AUTO_INCREMENT,
+   `client_id` int NOT NULL,
+   `first_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci DEFAULT NULL,
+   `last_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci DEFAULT NULL,
+   `gender` enum('m','f') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+   `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL,
+   `language` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+   `contact_style` enum('formal','informal') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'formal',
+   `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci DEFAULT NULL,
+   `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   `deletion_date` datetime DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `uidx_email_client_id` (`email`, `client_id`),
+   KEY `fk_client_accounts_client_id` (`client_id`),
+   CONSTRAINT `fk_client_accounts_client_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `client_id` int NOT NULL,
+  `employee_id` int NOT NULL,
+  `contact_first_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci DEFAULT NULL,
+  `contact_last_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci DEFAULT NULL,
+  `contact_gender` enum('m','f') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `contact_email` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL,
+  `contact_language` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL,
+  `contact_style` enum('formal','informal') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'formal',
+  `first_upsell_email_sent` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'FALSE',
+  `second_upsell_email_sent` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'FALSE',
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `client_account_created` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'FALSE',
+  `order_confirmation_sent` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'FALSE',
+  PRIMARY KEY (`id`),
+  KEY `fk_orders_client_id` (`client_id`),
+  CONSTRAINT `fk_orders_client_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 CREATE TABLE `prospect_mail_domains` (
     `id` int NOT NULL AUTO_INCREMENT,
     `mail_domain` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -54,6 +166,51 @@ CREATE TABLE `prospect_mail_domains` (
     CONSTRAINT `fk_prospect_mail_domains_review_employee_id` FOREIGN KEY (`review_employee_id`) REFERENCES `employees` (`id`),
     INDEX `idx_mail_domain` (`mail_domain`),
     INDEX `idx_language_registrant_country` (`language`, `registrant_country`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `search_terms` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `search_term` varchar(750) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `country` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL,
+    `detected_language` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci DEFAULT NULL,
+    `language_check_date` datetime DEFAULT NULL,
+    `listing_probability` enum('low','medium', 'high') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `update_metrics_more_frequently` enum('FALSE','TRUE') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'FALSE',
+    `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_connections_check_date` datetime DEFAULT NULL,
+    `last_listing_probability_check_date` datetime DEFAULT NULL,
+    `last_metrics_update_date` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uidx_country_search_term` (`country`, `search_term`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `search_term_metrics` (
+   `id` int NOT NULL AUTO_INCREMENT,
+   `search_term_id` int NOT NULL,
+   `creation_date` date NOT NULL,
+   `search_volume` int DEFAULT NULL,
+   `cpc` decimal(8,2) DEFAULT NULL,
+   `competition` decimal(8,2) DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   KEY `fk_search_term_metrics_search_term_id` (`search_term_id`),
+   CONSTRAINT `fk_search_term_metrics_search_term_id` FOREIGN KEY (`search_term_id`) REFERENCES `search_terms` (`id`),
+   UNIQUE KEY `uidx_search_term_id_creation_date` (`search_term_id`,`creation_date`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `search_term_connections` (
+   `id` int NOT NULL AUTO_INCREMENT,
+   `origin_search_term_id` int NOT NULL,
+   `destination_search_term_id` int NOT NULL,
+   `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (`id`),
+   KEY `fk_search_term_connections_origin_search_term_id` (`origin_search_term_id`),
+   CONSTRAINT `fk_search_term_connections_origin_search_term_id` FOREIGN KEY (`origin_search_term_id`) REFERENCES `search_terms` (`id`),
+   KEY `fk_search_term_connections_destination_search_term_id` (`destination_search_term_id`),
+   CONSTRAINT `fk_search_term_connections_destination_search_term_id` FOREIGN KEY (`destination_search_term_id`) REFERENCES `search_terms` (`id`),
+   UNIQUE KEY `uidx_origin_search_term_id_destination_search_term_id` (`origin_search_term_id`,`destination_search_term_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 # mysql users
