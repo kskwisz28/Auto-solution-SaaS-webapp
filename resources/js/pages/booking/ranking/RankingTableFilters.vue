8cc917b70ffa15ab855c2f7f6294ff4b115a7d99
kskwisz28 <kskwisz2853@gmail.com>
2022-12-21 23:38:28 +0100

    improved filters on mobile view

--- resources/js/pages/booking/ranking/RankingTableFilters.vue
+++ resources/js/pages/booking/ranking/RankingTableFilters.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex items-center justify-between p-7 pb-6">
-        <div class="relative md:max-w-sm self-start">
+    <div class="flex items-start justify-between flex-col md:flex-row gap-y-5 p-7 pb-6">
+        <div class="relative w-full md:max-w-xs lg:max-w-sm self-start">
             <svg class="w-6 h-6 text-zinc-400 absolute left-3 top-3" width="32" height="32" viewBox="0 0 24 24">
                 <path fill="currentColor" d="m21 19l-5.154-5.154a7 7 0 1 0-2 2L19 21l2-2zM5 10c0-2.757 2.243-5 5-5s5 2.243 5 5s-2.243 5-5 5s-5-2.243-5-5z"/>
             </svg>
@@ -8,7 +8,7 @@
             <Input placeholder="Filter keywords" v-model="keyword" @change="debouncedFilter" class="text-zinc-900 text-base pl-11"/>
         </div>
 
-        <div class="flex-1 px-4 flex flex-wrap gap-3">
+        <div class="flex-1 md:px-4 flex flex-wrap gap-3">
             <AppliedFilter v-if="filters.mustContainUrl.value"
                            @removed="toggleMustContainUrl">
                 contains URL
@@ -39,7 +39,7 @@
             </AppliedFilter>
         </div>
 
-        <div ref="filtersDropdown" class="dropdown dropdown-end self-start">
+        <div ref="filtersDropdown" class="dropdown dropdown-end self-end md:self-start">
             <label tabindex="0" class="btn btn-ghost text-sm">
                 <svg class="w-4 h-4 stroke-current mr-2" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M4.25 5.61C6.27 8.2 10 13 10 13v6c0 .55.45 1 1 1h2c.55 0 1-.45 1-1v-6s3.72-4.8 5.74-7.39A.998.998 0 0 0 18.95 4H5.04c-.83 0-1.3.95-.79 1.61z"/></svg>
                 Filters
